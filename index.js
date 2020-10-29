156f6d81798d0801c8f5c81127b88acdfee535f8
Russell Waters <45366908+rwaters210@users.noreply.github.com>
2020-10-29 12:35:15 -0500

    Update index.js
    Added the Manga database initialized as dict and the getMangaList method to return the title of the manga based off the number score from their answers. ( the variable n is used to record their score).

--- index.js
+++ index.js
@@ -5,16 +5,25 @@ function success(data) {
     $("#answer").text("Answer=" + data);
 }
 
-//function to prompt user
-function doQuestions() {
-    console.log("Ask the doQuestions")
+
+let dict = { 
+    one: {
+        Title: "My Hero Academia, The Promise Neverland, & Naruto", 
+        Author: "The Author", 
+        Published: "The Published Date" 
+    },
     
+    two: {
+        Title: "The Title 2", 
+        Author: "The Author 2", 
+        Published: "The Published Date 2" 
+    }
     
-}
+            };
 
 
 function hide() {
-     var x = document.getElementById("Questions");
+     let x = document.getElementById("Questions");
       if (x.style.display === "none") {
         x.style.display = "block";
       } else {
@@ -28,6 +37,76 @@ function getList() {
     alert("Here is the list of books:.....");
     console.log("List Retrieved");
 }
+
+function getSelectValues() {
+    var selectedValue1 = document.getElementById("list1").value;
+    var selectedValue2 = document.getElementById("list2").value;
+    var selectedValue3 = document.getElementById("list3").value;
+    
+    let n = 0;
+    switch(selectedValue1) {
+        case "Teen":
+            n += 1;
+        break;
+        case "Young Adult":
+            n += 2;
+        break;
+        case "Adult":
+            n += 3;
+        break;    
+        default:
+            //nothing
+    }
+    
+    switch(selectedValue2) {
+        case "Action":
+            n += 1;
+        break;
+        case "Thriller":
+            n += 2;
+        break;
+        case "Fantasy":
+            n += 3;
+        break;    
+        default:
+            //nothing
+    }
+    
+    switch(selectedValue3) {
+        case "Short":
+            n += 1;
+        break;
+        case "Medium":
+            n += 2;
+        break;
+        case "Long":
+            n += 3;
+        break;    
+        default:
+            //nothing
+    }
+    
+    //let x = getMangalist(n);
+    
+    document.getElementById("MangaList").innerHTML = getMangalist(n);
+    
+    console.log(n);
+    
+    console.log("Values Selected");
+}
+
+function getMangalist(number) {
+    if(number == 6) {
+        //let x = dict[2];
+        //let answer = "";
+        //answer += x['Title'];
+        console.log(dict[one].Title);
+        return (dict[one].Title + dict[one].Author);
+    } else {
+        return "Other List...";
+    }
+}
+
 
 
 //start button that intiates conversation
