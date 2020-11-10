d9a5234639a9ff03e49c4453b096f7f8bf177758
Russell Waters <45366908+rwaters210@users.noreply.github.com>
2020-11-10 12:35:47 -0600

    Update process.js

--- process.js
+++ process.js
@@ -1,3 +1,5 @@
+console.log("Hellllloooooo!!!!");
+
 const express = require('express');
 const app = express();
 let port = process.env.PORT;
@@ -10,10 +12,58 @@ function index(req, res) {
 }
 
 
+//create array of dictionary for manga database
 
+var lib = { 
+    1: {
+        Title: "My Hero Academia, The Promise Neverland, & Naruto", 
+        Author: "The Author", 
+        Published: "The Published Date" 
+    },
+    
+    2: {
+        Title: "The Title 2", 
+        Author: "The Author 2", 
+        Published: "The Published Date 2" 
+    }
+    
+};
 
+//Insert getMangalist function;
+//insert dictionary/ api
 
+function getMangalist(req, res) {
+    let x = lib[1];
+    let y = lib[2];
+    
+    let answer = "Other List";
+    
+    let n = req.params.number;
+    
+    if(n == 6) {
+        answer += x['Title'];
+        //console.log(dict.one.Title);
+        //return answer;
+        res.send(answer);
+        console.log(answer);
+        return;
+    } else if (n == 3){
+        answer += y['Title'];
+        //return answer;
+        res.send(answer);
+        console.log(answer);
+        return;
+    } else {
+        res.send(answer);
+    }
+}
 
+app.use(express.static("static"));
+
+app.get('/', index);
+
+// Calling "/getMangalist/..." invokes the calculate function
+app.get('/getMangalist/number/:number', getMangalist);
 
 //Start Express listening at the given port
 app.listen(port, function() {
