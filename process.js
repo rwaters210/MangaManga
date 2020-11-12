66bd9e08bd0509ec82ec6bd0850b50cb2437e9f8
gmoody2020 <gmoody@my.tnstate.edu>
2020-11-12 05:50:28 +0000

    update process.js

--- process.js
+++ process.js
@@ -17,166 +17,164 @@ function index(req, res) {
 var lib = { 
     1: {
         Title: "My Hero Academia, The Promise Neverland, & Naruto", 
-        Author: "The Author", 
-        Published: "The Published Date" 
+        Author: "Kohei Horikoshi, Kaiu Shirai, Masashi Kishimoto", 
+        Published: "2014, 2016, 1999" 
     },
     
     2: {
-        Title: "The Title 2", 
-        Author: "The Author 2", 
-        Published: "The Published Date 2" 
+        Title: "Needless, Aiki, Gamaran", 
+        Author: "Kami Imai, Isutoshi, Nakamaru Yousuke", 
+        Published: "2003, 2013, 2013" 
     },
     
     3: {
-        Title: "The Title 3", 
-        Author: "The Author 3", 
-        Published: "The Published Date 3" 
+        Title: "Hunter x Hunter, Attack on Titan , Berserk", 
+        Author: "Yoshiro Togashi, Hajime Isayama, Kentaro Miura", 
+        Published: "1998, 2009, 1990" 
     },
     
     4: {
-        Title: "The Title 4", 
-        Author: "The Author 4", 
-        Published: "The Published Date 4" 
+        Title: "Hikaru no GO, Barakamon, Yotsuba&I", 
+        Author: "Yumi Hotta and Takeshi Obata, Satsuki Yoshino, Kiyohiko Azuma", 
+        Published: "1999, 2009, 2003" 
     },
     
     5: {
-        Title: "The Title 5", 
-        Author: "The Author 5", 
-        Published: "The Published Date 5" 
+        Title: "Haikyu, Blue exorcist, One Punch Man", 
+        Author: "Haruichi Furudate, Kazue Kato, Yusuke Murata", 
+        Published: "2012, 2009, 2012" 
     },
     
     6: {
-        Title: "The Title 6", 
-        Author: "The Author 6", 
-        Published: "The Published Date 6" 
+        Title: "Bleach, Tokyo ghoul, Fairy Tail", 
+        Author: "Tite Kubo, Sui Ishida, Hiro Mashima", 
+        Published: "2001, 2001, 2006" 
     },
     
     7: {
-        Title: "The Title 7", 
-        Author: "The Author 7", 
-        Published: "The Published Date 7" 
+        Title: "Soul Eater, Alive: Saishuu Shinkateki Shounen, Psyren", 
+        Author: "Atsushi Okubo, Tadashi Kawashima, Toshiaki Iwashiro", 
+        Published: "2004, 2016, 2007" 
     },
     
     8: {
-        Title: "The Title 8", 
-        Author: "The Author 8", 
-        Published: "The Published Date 8" 
+        Title: "Parasyte, Vinland Saga, Bomber Girl", 
+        Author: "Hitoshi Iwaaki, Makoto Yukimura, Makoto Niwano", 
+        Published: "2014, 2005, 1994" 
     },
     
     9: {
-        Title: "The Title 9", 
-        Author: "The Author 9", 
-        Published: "The Published Date 9" 
+        Title: "Pluto, Freesia, Hurejasik", 
+        Author: "Naoki Urasawa, Jiro Matsumoto, kazushi Hagiwara ", 
+        Published: "2004, 2001, 2000" 
     },
     
     10:{
-        Title: "The Title 10", 
-        Author: "The Author 10", 
-        Published: "The Published Date 10" 
+        Title: "Tajuu Jinkaku Tantei Pyscho, Homunculus, Monster", 
+        Author: "Eiji Otsuka, Hideo Yamamoto, Naoki Urasawa", 
+        Published: "2007, 2003, 1994" 
     },
     
     11:{
-        Title: "The Title 11", 
-        Author: "The Author 11", 
-        Published: "The Published Date 11" 
+        Title: "Life is Money, Route End, Doubt", 
+        Author: "Asaniji Teru, Nakagawa Kaini, Yoshiki Tonogai", 
+        Published: "2011, 2017, 2007" 
     },
     
     12:{
-        Title: "The Title 12", 
-        Author: "The Author 12", 
-        Published: "The Published Date 12" 
+        Title: "Pigpen, Ikigami: the Ultimate Limit, Uriah", 
+        Author: "Kim Carnby, Motoro Mase, Toffuo", 
+        Published: "2015, 2009, 2019" 
     },
     
     13:{
-        Title: "The Title 13", 
-        Author: "The Author 13", 
-        Published: "The Published Date 13" 
+        Title: "Save Me, Your Throne, Usogui", 
+        Author: "1230, SAM, Toshio Sako", 
+        Published: "2019, 2020, 2005" 
     },
     
     14:{
-        Title: "The Title 14", 
-        Author: "The Author 14", 
-        Published: "The Published Date 14" 
+        Title: "Green Blood, Future Diary, Kishibe Rohan Wa Ugokanai", 
+        Author: "Kakizaki Masasumi, Sakae Esuno, Hirohiko Araki", 
+        Published: "2011, 2006, 1997" 
     },
     
     15:{
-        Title: "The Title 15", 
-        Author: "The Author 15", 
-        Published: "The Published Date 15" 
+        Title: "Angels of Death, Fort of Apocalypse, Summertime Rendering", 
+        Author: "Kudan Naduka, Yuu Kuraishi, Yasuki Tanaka", 
+        Published: "2016, 1982, 2018" 
     },
     
     16:{
-        Title: "The Title 16", 
-        Author: "The Author 16", 
-        Published: "The Published Date 16" 
+        Title: "Bastard, Akumetsu, Sweet Home", 
+        Author: "Kazushi Hagiwara, Yoshiaki Tabata, Youngchan Hwang", 
+        Published: "1992, 2002, 2018" 
     },
     
     17:{
-        Title: "The Title 17", 
-        Author: "The Author 17", 
-        Published: "The Published Date 17" 
+        Title: "Overlord, Log Horizon, Mononoke Hime", 
+        Author: "Kugane Maruyama, Daisuke Umezu, Hayao Miyazaki", 
+        Published: "2012, 2013, 1997" 
     },
     
     18:{
-        Title: "The Title 18", 
-        Author: "The Author 18", 
-        Published: "The Published Date 18" 
+        Title: "Fate/Zero, Overlord II, No Game No Life", 
+        Author: "Gen Urobuchi, Hugin Miyama, Yu Kamiya", 
+        Published: "2011, 2012, 2012" 
     },
     
     19:{
-        Title: "The Title 19", 
-        Author: "The Author 19", 
-        Published: "The Published Date 19" 
+        Title: "Full Metal Alchemist, Death Note ,One Piece", 
+        Author: "Hiromu Arakawa, Tsugumi Ohba, Eiichiro Oda", 
+        Published: "2001, 2003, 1997" 
     },
     
     20:{
-        Title: "The Title 20", 
-        Author: "The Author 20", 
-        Published: "The Published Date 20" 
+        Title: "Superior, Superior Cross, Helck", 
+        Author: "Ichtys, Ichtys, Nanao Nanaki", 
+        Published: "2004, 2004, 2014" 
     },
     
     21:{
-        Title: "The Title 21", 
-        Author: "The Author 21", 
-        Published: "The Published Date 21" 
+        Title: "Dungeon Meshi, Shounen Oujo, Mahoutsukai no Yome", 
+        Author: "Ryoko Kui, Utako Yukihiro, Kore Yamazaki", 
+        Published: "2014, 2011, 2013" 
     },
     
     22:{
-        Title: "The Title 22", 
-        Author: "The Author 22", 
-        Published: "The Published Date 22" 
+        Title: "Nanastu no Taizai, Fairy Tail, Mahou Sensei Negima!", 
+        Author: "Nakaba Suzuki, Hiro Mashima, Ken Akamatsu", 
+        Published: "2011, 2011, 2003" 
     },
     
     23:{
-        Title: "The Title 23", 
-        Author: "The Author 23", 
-        Published: "The Published Date 23" 
+        Title: "Niehime to Kemono no Ou, Drifters, Houseki no Kuni", 
+        Author: "Yu Tomofuji, Kouta Hirano, Haruko Ichikawa", 
+        Published: "2015, 2009, 2012" 
     },
     
     24:{
-        Title: "The Title 24", 
-        Author: "The Author 24", 
-        Published: "The Published Date 24" 
+        Title: "Houshin Engi, Dorohedoro, claymore", 
+        Author: "Ryu Fujisaki, Q Hayashida, Norihiro Yagi", 
+        Published: "2005, 2000, 2001" 
     },
     
     25:{
-        Title: "The Title 25", 
-        Author: "The Author 25", 
-        Published: "The Published Date 25" 
+        Title: "Naruto Shippuden, Made in Abyss, Inuyasha ", 
+        Author: "Masashi Kishimoto, Akihito Tsukushi, Rumiko Takahashi",
+        Published: "1999, 2012, 1996" 
     },
     
     26:{
-        Title: "The Title 26", 
-        Author: "The Author 26", 
-        Published: "The Published Date 26" 
+        Title: "Fruits Basket, Re:Creators, Maoyuu Maou Yuusha", 
+        Author: "Natsuki Takaya, Shizuka Kurosaki, Mamare Touno", 
+        Published: "1998, 2017, 2011" 
     },
     
     27:{
-        Title: "The Title 27", 
-        Author: "The Author 27", 
-        Published: "The Published Date 27" 
-=======
->>>>>>> d9a5234639a9ff03e49c4453b096f7f8bf177758
+        Title: "Dragon Ball Z, Assassination Classroom, Jojo's Bizarre adventure", 
+        Author: "Akira Toriyama, Yusei Matsui, Hirohiko Araki ", 
+        Published: "1984, 2012, 1987" 
     }
     
 };
